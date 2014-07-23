95186157c762028f23ed855b07697ad9a5757ebc
Thor77 <xXThor77Xx@gmail.com>
2014-07-23 14:02:23 +0200

    added pluginloader

---
+++ pluginloader.py
@@ -0,0 +1,43 @@
+import importlib
+import os
+
+class PluginLoader:
+
+    def __init__(self, path):
+        self.path = path
+        self.classes = []
+        self.modules = []
+        self.path_module = {}
+        self.load_plugins()
+
+    def __iter__(self):
+        for cl in self.classes:
+            yield cl
+
+    def find_plugins(self):
+        module_paths = []
+        for (dirpath, dirs, files) in os.walk(self.path):
+            if not len(files) <= 0 and any(f.endswith('.py') for f in files):
+                module_paths.append(dirpath.replace('\\', '.'))
+        return module_paths
+
+    def load_plugins(self):
+        module_paths = self.find_plugins()
+        for path in module_paths:
+            module = importlib.import_module(path)
+            self.modules.append(module)
+            self.path_module[path] = module
+        self.update_classes()
+
+    def update_classes(self):
+        self.classes = Plugin.__subclasses__()
+
+    def get_module(self, cl):
+        classes_modules = dict(zip(self.classes, self.modules))
+        if cl in classes_modules:
+            return classes_modules[cl]
+        else:
+            raise NameError('Invalid class!')
+
+class Plugin:
+    pass
